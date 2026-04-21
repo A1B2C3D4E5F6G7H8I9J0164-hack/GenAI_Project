a35dc2823e8dac9189b76046ab292d16506699c4
CosmicMagnetar <krishna.2024@nst.rishihood.edu.in>
2026-04-21 10:43:30 +0530

    check changes

--- End_sem/frontend/src/pages/BatchProcess.jsx
+++ End_sem/frontend/src/pages/BatchProcess.jsx
@@ -171,7 +171,7 @@ const BatchProcess = () => {
                   {result.mae !== undefined && (
                     <StatCard
                       title="Mean absolute error"
-                      value={`${result.mae.toFixed(4)}`}
+                      value={`${100*result.mae.toFixed(4)}`}
                       color="magenta"
                       className="hidden lg:flex"
                     />
